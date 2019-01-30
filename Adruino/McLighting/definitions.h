fa63172f98bab6db5afd043aec65212cb4d22fd3
BPoH_Voodoo <mirco@dzillum.de>
2019-01-30 12:49:48 +0100

    Update to use DMA
    Update to use DMA

--- Adruino/McLighting/definitions.h
+++ Adruino/McLighting/definitions.h
@@ -1,9 +1,9 @@
-//#define USE_WS2812FX_DMA      // Uses PIN is ignored & set to RX/GPIO3  Uses WS2812FX, see: https://github.com/kitesurfer1404/WS2812FX
+#define USE_WS2812FX_DMA      // Uses PIN is ignored & set to RX/GPIO3  Uses WS2812FX, see: https://github.com/kitesurfer1404/WS2812FX
 //#define USE_WS2812FX_UART1     // Uses PIN is ignored & set to D4/GPIO2  Uses WS2812FX, see: https://github.com/kitesurfer1404/WS2812FX
 //#define USE_WS2812FX_UART2     // Uses PIN is ignored & set to TX/GPIO1  Uses WS2812FX, see: https://github.com/kitesurfer1404/WS2812FX
 
 // Neopixel
-#define PIN 15             // PIN (15 / D8) where neopixel / WS2811 strip is attached 
+#define PIN 3              // PIN (15 / D8) where neopixel / WS2811 strip is attached 
 #define NUMLEDS 12         // Number of leds in the strip 
 #define BUILTIN_LED 2      // ESP-12F has the built in LED on GPIO2, see https://github.com/esp8266/Arduino/issues/2192
 #define BUTTON  14         // Input pin (14 / D5) for switching the LED strip on / off, connect this PIN to ground to trigger button.
